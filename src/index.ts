b4027f626ce3783fd5e1d9ee1781cd6ef1bd5d99
anikas <22bec020@nith.ac.in>
2025-07-15 20:05:15 +0530

    ws 2

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,37 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
-wss.on("connection", function (socket) {
+/*wss.on("connection", function (socket) {
   console.log("user  connected");
   setInterval(() => {
     socket.send("hello user :)");
   }, 5000);
+});
+*/ /*
+wss.on("connection", function (socket) {
+  console.log("user connected");
+
+  socket.on("message", (e) => {
+    const message = e.toString(); // Convert to string explicitly
+    console.log("Received:", message);
+
+    if (message === "ping") {
+      console.log("Sending pong");
+      socket.send("pong");
+    }
+  });
+
+  // Optional: Handle connection close
+  socket.on("close", () => {
+    console.log("user disconnected");
+  });
+});
+*/
+wss.on("connection", function (socket) {
+  console.log("user connected");
+
+  socket.on("message", (e) => {
+    if (e.toString() === "ping") {
+      socket.send("pong");
+    }
+  });
 });
