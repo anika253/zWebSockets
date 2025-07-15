3549fc9559f03d2746fd0da585ecc2f755e4073e
anikas <22bec020@nith.ac.in>
2025-07-15 18:53:36 +0530

    web s

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 8080 });
+wss.on("connection", (ws) => {
+  console.log("New client connected");
+
+  ws.on("message", (message) => {
+    console.log(`Received message: ${message}`);
+
+    ws.send(`Server received: ${message}`);
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
