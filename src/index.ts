132aca31eae27457952978bce0be09b049ce0fe1
anikas <22bec020@nith.ac.in>
2025-07-15 19:01:18 +0530

    postman check

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
 import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
-wss.on("connection", (ws) => {
-  console.log("New client connected");
-
-  ws.on("message", (message) => {
-    console.log(`Received message: ${message}`);
-
-    ws.send(`Server received: ${message}`);
-  });
-
-  ws.on("close", () => {
-    console.log("Client disconnected");
-  });
+wss.on("connection", function (socket) {
+  console.log("user  connected");
+  setInterval(() => {
+    socket.send("hello user :)");
+  }, 5000);
 });
