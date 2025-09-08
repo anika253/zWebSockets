f6d232dd193d1b50e8dcb8180c574457154b24c9
anikas <22bec020@nith.ac.in>
2025-09-08 10:22:21 +0530

    Create room

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 let userCount = 0;
-let allsockets: WebSocket[] = [];
-// WebSocket is a native function ..not always but here
+interface User {
+  socket: WebSocket;
+  room: string;
+}
+let allsockets: User[] = [];
+{
+    {socket : socket , room : "room1"},
+    {socket: socket , room : "room2"},
+    {socket: socket , room : "room3"},
+}
+// WebSocket is a native function ..not here
 
 // runs on every new connection
+/*
 wss.on("connection", (socket) => {
   allsockets.push(socket);
   console.log("user connected");
@@ -17,6 +27,12 @@ wss.on("connection", (socket) => {
       const s = allsockets[i];
       s.send(e.toString() + " from server to all clients");
     }
+  });
+  socket.on("disconnect", () => {
+    allsockets = allsockets.filter((s) => s !== socket);
+    userCount--;
+    console.log("user disconnected");
+    console.log("user count:", userCount);
   });
 });
 // how server responds to client
@@ -33,9 +49,32 @@ only diff bw wsockets and http is wsockets have persistent connection
 and https has get , posyt, put , delete methods
 web sockets have onmessage, onopen, onclose methods
 how to receive and send msgs from server ?
+js runtime :
+browser js -.no native http model
+node js ->native http model , has naive websocket server also but we are using their library here 
+bunjs -> native http model 
+either join a room or create a room here in the chat application
+type :join// what to do 
+payload : 
+{
+  roomid : "abc",
+  name : "anika"
+}
+
+type :message
+payload :
+{
+  roomid : "abc",
+  name : "anika",
+  message : "hello"
+}
+
+server tell the user the msg to frwd it to every one
+
 
 
 /*
 
 
 **/
+// now i wanna chat bw multiple clients nd rooms here
